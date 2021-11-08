565e3185d35680dc319b852df403f469d448b551
210839 <93528711+210839@users.noreply.github.com>
2021-11-08 13:32:33 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ camera.attachControl(canvas, true);
 //   }
 // }
 var i = 0;
-for (i = 0; i < 3; i++) {
+for (i = 0; i < 5; i++) {
   var sphere = MeshBuilder.CreateCylinder(
     "freza",
     { diameter: 0.2, height: 3 },
@@ -68,7 +68,7 @@ for (i = 0; i < 3; i++) {
   sphere.position.x = i;
   if (i === 2) {
     var Mat = new StandardMaterial("sedy", scene);
-    Mat.diffuseColor = new Color3(0.5, 0.5, 0.6);
+    Mat.diffuseColor = new Color3(0.8, 0.6, 0.6);
     sphere.material = Mat;
   }
 }
