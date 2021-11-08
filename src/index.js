f3befe2b34bd86731633cec3c8b43c7699a1e622
stepanz7 <93528573+stepanz7@users.noreply.github.com>
2021-11-08 13:32:32 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ camera.attachControl(canvas, true);
 //   }
 // }
 var i = 0;
-for (i = 0; i < 3; i++) {
+for (i = 0; i < 8; i++) {
   var sphere = MeshBuilder.CreateCylinder(
     "freza",
     { diameter: 0.2, height: 3 },
@@ -68,7 +68,7 @@ for (i = 0; i < 3; i++) {
   sphere.position.x = i;
   if (i === 2) {
     var Mat = new StandardMaterial("sedy", scene);
-    Mat.diffuseColor = new Color3(0.5, 0.5, 0.6);
+    Mat.diffuseColor = new Color3(0.9, 0.9, 0.4);
     sphere.material = Mat;
   }
 }
